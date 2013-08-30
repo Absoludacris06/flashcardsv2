78f42c20df1916d729b080e71bf3e789791401f4
Apprentice <apprentice@dbc15.local>
2013-08-30 15:25:04 -0700

    added navbar frontpage auth

--- app/controllers/sessions.rb
+++ app/controllers/sessions.rb
@@ -1,4 +1,4 @@
-post '/user' do 
+post '/user/new' do 
   @user = User.create(params)
   if @user.save
     session["user_id"] = @user.id
@@ -6,3 +6,18 @@ post '/user' do
     @error = "Please try again"
   end
 end
+
+post '/user' do
+
+  @user = User.find_by_email(params[:email])
+  
+  if @user.password == params[:password]
+    session["user_id"] = @user.id
+  else
+    @invalid = "Invalid password and username combination"
+  end
+
+  erb :index
+
+end
+
