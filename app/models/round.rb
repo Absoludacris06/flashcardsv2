fa9269ee8ca30384bdbe545998aa08745d273f0a
Apprentice <apprentice@dbc15.local>
2013-08-31 15:03:37 -0700

    new round model - unfinished

--- app/models/round.rb
+++ app/models/round.rb
@@ -6,33 +6,44 @@ class Round < ActiveRecord::Base
   has_many :guesses
 
 
-  def self.definition(id)
-    definition = []
+  # def self.definition(id)
+  #   definition = []
+  #   self.find(id).guesses.each do |guess_obj|
+  #     definition << guess_obj.card.definition
+  #   end
+  #   definition
+  # end
+
+  # def self.answer(id)
+  #   answer = []
+  #   self.find(id).guesses.each do |guess_obj|
+  #     answer << guess_obj.card.answer
+  #   end
+  #   answer
+  # end
+
+  # def self.response(id)
+  #   responses = []
+  #   self.find(id).guesses.each do |guess_obj|
+  #     guess_obj.correctness == 0
+  #     responses << guess_obj.responses
+  #   end
+  #   response
+  # end
+
+  # def self.scores
+  #   Round.definition().zip(Round.answer, Round.response)
+  # end
+
+  def self.results
+    results = []
     self.find(id).guesses.each do |guess_obj|
-      definition << guess_obj.card.definition
+      results << guess_obj.card
     end
-    definition
+  results 
   end
 
-  def self.answer(id)
-    answer = []
-    self.find(id).guesses.each do |guess_obj|
-      answer << guess_obj.card.answer
-    end
-    answer
-  end
-
-  def self.response(id)
-    responses = []
-    self.find(id).guesses.each do |guess_obj|
-      guess_obj.correctness == 0
-      responses << guess_obj.responses
-    end
-    response
-  end
-
-  def self.scores
-    Round.definition().zip(Round.answer, Round.response)
-  end
 
 end
+
+
