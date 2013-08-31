0215969a14f6bc01688be44598748ef2a7d176a3
DBC-Apprentice <dbc@dbc.com>
2013-08-30 19:11:03 -0700

    fix session bug where too much mem was used

--- app/controllers/game.rb
+++ app/controllers/game.rb
@@ -1,11 +1,9 @@
 
 post ('/round') do
-  session[:deck_id] = params[:deck_id]
-  @round = Round.create(user_id: session[:user_id], deck_id: session[:deck_id])
+  @round = Round.create(user_id: session[:user_id], deck_id: params[:deck_id])
   session[:round_id] = @round.id
-  session[:deck] = Deck.find(@round.deck_id).cards.shuffle
-  session[:card] = session[:deck].pop
-  @card = session[:card]
+  session[:deck] = Deck.shuffled_card_ids(params[:deck_id])
+  @card = Card.find(session[:deck].pop)
   erb :guess
 end
 
@@ -13,14 +11,13 @@ post ('/guess') do
   if session[:deck].empty?
     erb :results
   else
-    if session[:card].answer == params[:guess] #turn into model method
+    if Card.find(params[:card_id]).answer == params[:guess] #turn into model method
       @correct = 1
     else
       @correct = 0
     end
-    @guess = Guess.create(card_id: session[:card].id, round_id: session[:round_id], correctness: @correct)
-    session[:card] = session[:deck].pop
-    @card = session[:card]
+    @guess = Guess.create(card_id: params[:card_id], round_id: session[:round_id], correctness: @correct)
+    @card = Card.find(session[:deck].pop)
     erb :guess
   end
 end
--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,3 +1,12 @@
 class Deck < ActiveRecord::Base
   has_many :cards, dependent: :destroy
+
+  def self.shuffled_card_ids(deck_id)
+    ids = []
+    cards = Deck.find(deck_id).cards
+    cards.each do |card|
+      ids << card.id
+    end
+    ids.shuffle
+  end
 end
